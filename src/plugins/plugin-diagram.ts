3fa5907c546380c3a61859d1a8ebd6cf442f4455
AngryPowman <138001655@qq.com>
2016-07-26 00:03:42 +0800

    Extend interface

--- src/plugins/plugin-diagram.ts
+++ src/plugins/plugin-diagram.ts
@@ -2,19 +2,36 @@
 
 module MarkdownSuperset {
   export class PluginDiagram implements MarkdownExtraPlugin {
+    public initialize(): void {
+      mermaid.initialize({
+        startOnLoad: true,
+        flowchart: {
+          useMaxWidth: false
+        },
+        sequenceDiagram: {
+          useMaxWidth: false,
+          bottomMarginAdj: 50
+        }
+      });
+    }
 
     public render(markdown: string): string {
       console.log("Rendering with diagram ...");
       return markdown;
     }
 
-    public renderDiagram(code: string): string {
+    public update(): void {
+      mermaid.init({}, ".mermaid");
+    }
+
+    // ========================================================
+    public codeRender(code: string): string {
       console.log("Parsing diagram token in code block ...");
       return '<div class="mermaid">' + code + '</div>';
     }
 
     public tryParse(code: string): boolean {
-      if (code.match(/^sequenceDiagram/) || code.match(/^graph/)) {
+      if (code.match(/^\s*sequenceDiagram/) || code.match(/^\s*graph\s/) || code.match(/^\s*gantt/)) {
         return true;
       }
 
